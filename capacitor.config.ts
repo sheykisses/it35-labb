9b3fb6e888e66ccc87bdfbbe15529bb8b0df390b
sheykisses <dancilhershey65@gmail.com>
2025-03-03 21:24:14 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'it35-labb',
+  webDir: 'dist'
+};
+
+export default config;
