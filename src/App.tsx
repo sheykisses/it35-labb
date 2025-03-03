e32d2832a9c77a400e508d7614967c18e064cb56
sheykisses <dancilhershey65@gmail.com>
2025-03-03 21:31:23 +0800

    Reconfigure vite.config.ts, App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,11 +39,11 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
+        <Route exact path="/it35-labb/home">
           <Home />
         </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
+        <Route exact path="/it35-labb/">
+          <Redirect to="/it35-labb/home" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     react(),
     legacy()
   ],
+  base: "/it35-labb",
   test: {
     globals: true,
     environment: 'jsdom',
